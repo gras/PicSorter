20c6f1a5d0b192d83549bcf9915d0063b2c18ef5
gras <github@gras.org>
2021-02-03 21:02:49 -0500

    JPGs are working!

--- PicSorter.py
+++ PicSorter.py
@@ -1,13 +1,23 @@
 from PIL import Image
+import os
 
 baseDir = '/data/'
-fileDir = '/photos/testarea/IMG_4994.JPG'
-photo = Image.open(baseDir + fileDir)
-pDateTime = photo.getexif()[36867]
-pDate = pDateTime.split(' ')[0].split(':')
-yearDir = pDate[0]
-monthDir = pDate[1] + '-' + pDate[2]
+fileDir = '/photos/iphone/'
 
-
-print(yearDir)
-print(monthDir)
+for entry in os.scandir(baseDir + fileDir):
+    if (entry.path.endswith(".MOV")):
+        print("skipping:  %s" % entry.path)
+    elif (entry.path.endswith(".PNG")):
+        print("skipping:  %s" % entry.path)
+    elif (entry.path.endswith(".ini")):
+        print("skipping:  %s" % entry.path)
+    elif (entry.path.endswith(".AAE")):
+        print("skipping:  %s" % entry.path)
+    else:
+        print("testing:  %s" % entry.path)
+        photo = Image.open(entry.path)
+        pDateTime = photo.getexif()[36867]
+        pDate = pDateTime.split(' ')[0].split(':')
+        yearDir = pDate[0]
+        monthDir = pDate[1] + '-' + pDate[2]
+        print("%s %s %s" % (yearDir, monthDir, entry.path))
