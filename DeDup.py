55dc0bdd99e83393e19903699358fb60d1a1116a
gras <github@gras.org>
2021-02-09 17:10:10 -0500

    DeDup moves the 'deleted' file to a deleted/ directory

--- DeDup.py
+++ DeDup.py
@@ -2,13 +2,14 @@
 # python detect_and_remove.py --dataset dataset
 # python detect_and_remove.py --dataset dataset --remove 1
 
-# import the necessary packages
 from imutils import paths
 import numpy as np
 # import argparse
 import cv2
-# import os
+import shutil
 
+imagePaths = "/gras-data/photos/To_be_sorted/misc"
+deletePath = "/gras-data/photos/deleted"
 
 def dhash(image, hashSize=8):
     # convert the image to grayscale and resize the grayscale image,
@@ -38,7 +39,7 @@ args = vars(ap.parse_args())
 print("[INFO] computing image hashes...")
 # imagePaths = list(paths.list_images(args["dataset"]))
 
-imagePaths = list(paths.list_images("/gras-data/photos/To_be_sorted/misc"))
+imagePaths = list(paths.list_images(imagePaths))
 hashes = {}
 
 # loop over our image paths
@@ -58,32 +59,31 @@ for imagePath in imagePaths:
 for (h, hashedPaths) in hashes.items():
     # check to see if there is more than one image with the same hash
     if len(hashedPaths) > 1:
-        # initialize a montage to store all images with the same hash
-        montage = None
-
         # loop over all image paths with the same hash
         dups = []
         for p in hashedPaths:
             dups.append(p)
-            # load the input image and resize it to a fixed width
-            # and height
-            image = cv2.imread(p)
-            image = cv2.resize(image, (150, 150))
-
-            # if our montage is None, initialize it
-            if montage is None:
-                montage = image
-
-            # otherwise, horizontally stack the images
-            else:
-                montage = np.hstack([montage, image])
-        # show the montage for the hash
-        # print("[INFO] hash: {}".format(h))
-        cv2.imshow("Montage", montage)
         while len(dups) > 1:
             print("Which one to delete? (x means skip, q means Quit)")
+            # initialize a montage to store all images with the same hash
+            montage = None
             for i, p in enumerate(dups):
                 print(" %d : %s" % (i, p))
+                # load the input image and resize it to a fixed width
+                # and height
+                image = cv2.imread(p)
+                image = cv2.resize(image, (150, 150))
+
+                # if our montage is None, initialize it
+                if montage is None:
+                    montage = image
+
+                # otherwise, horizontally stack the images
+                else:
+                    montage = np.hstack([montage, image])
+            # show the montage for the hash
+            # print("[INFO] hash: {}".format(h))
+            cv2.imshow("Montage", montage)
             c = cv2.waitKey(0) % 256
             if c == ord('x'):
                 print("Skipping...")
@@ -97,18 +97,9 @@ for (h, hashedPaths) in hashes.items():
                     raise ValueError
 
                 print("removing: %s" % dups[num])
+                delName = dups[num][:-3] + 'DELETED'
+                print(delName)
+                shutil.move(dups[num], delName)
                 dups.remove(dups[num])
-                # remove file
-                # consider rebuilding montage
-
-
             except ValueError:
                 print("you pressed %s, which is invalid" % chr(c))
-
-        # otherwise, we'll be removing the duplicate images
-        # else:
-        #     # loop over all image paths with the same hash *except*
-        #     # for the first image in the list (since we want to keep
-        #     # one, and only one, of the duplicate images)
-        #     for p in hashedPaths[1:]:
-        #         os.remove(p)
