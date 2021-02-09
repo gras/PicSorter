b503fbfbc68ce258aca452d5d29f516fb7257e0b
gras <github@gras.org>
2021-02-08 20:19:58 -0500

    DeDup ready to delete the file

--- DeDup.py
+++ DeDup.py
@@ -58,32 +58,52 @@ for imagePath in imagePaths:
 for (h, hashedPaths) in hashes.items():
     # check to see if there is more than one image with the same hash
     if len(hashedPaths) > 1:
-        # check to see if this is a dry run
-        # if args["remove"] <= 0:
-        if True:
-            # initialize a montage to store all images with the same
-            # hash
-            montage = None
+        # initialize a montage to store all images with the same hash
+        montage = None
 
-            # loop over all image paths with the same hash
-            for p in hashedPaths:
-                # load the input image and resize it to a fixed width
-                # and height
-                image = cv2.imread(p)
-                image = cv2.resize(image, (150, 150))
+        # loop over all image paths with the same hash
+        dups = []
+        for p in hashedPaths:
+            dups.append(p)
+            # load the input image and resize it to a fixed width
+            # and height
+            image = cv2.imread(p)
+            image = cv2.resize(image, (150, 150))
 
-                # if our montage is None, initialize it
-                if montage is None:
-                    montage = image
+            # if our montage is None, initialize it
+            if montage is None:
+                montage = image
 
-                # otherwise, horizontally stack the images
-                else:
-                    montage = np.hstack([montage, image])
+            # otherwise, horizontally stack the images
+            else:
+                montage = np.hstack([montage, image])
+        # show the montage for the hash
+        # print("[INFO] hash: {}".format(h))
+        cv2.imshow("Montage", montage)
+        while len(dups) > 1:
+            print("Which one to delete? (x means skip, q means Quit)")
+            for i, p in enumerate(dups):
+                print(" %d : %s" % (i, p))
+            c = cv2.waitKey(0) % 256
+            if c == ord('x'):
+                print("Skipping...")
+                break
+            if c == ord('q'):
+                print("Quitting...")
+                exit('1')
+            try:
+                num = int(chr(c))
+                if num >= len(dups):
+                    raise ValueError
 
-            # show the montage for the hash
-            print("[INFO] hash: {}".format(h))
-            cv2.imshow("Montage", montage)
-            cv2.waitKey(0)
+                print("removing: %s" % dups[num])
+                dups.remove(dups[num])
+                # remove file
+                # consider rebuilding montage
+
+
+            except ValueError:
+                print("you pressed %s, which is invalid" % chr(c))
 
         # otherwise, we'll be removing the duplicate images
         # else:
